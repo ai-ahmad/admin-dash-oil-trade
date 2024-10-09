85e69bf426ad8a3ab17ab7da15f039e271918d63
ai-ahmad <ahmedovalmat8@gmail.com>
2024-10-09 16:48:24 +0500

    test

--- src/routes/ProductRouter.js
+++ src/routes/ProductRouter.js
@@ -1,76 +1,7 @@
 const express = require('express');
 const multer = require('multer');
 const mongoose = require('mongoose');
-
-// Define the product schema
-const productSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  category: {
-    type: String,
-    required: true,
-  },
-  rating: {
-    type: Number,
-    required: true,
-    default: 0,
-  },
-  price: {
-    type: Number,
-    required: true,
-  },
-  volume: {
-    type: Array,
-    required: true,
-  },
-  stock: {
-    type: Number,
-    required: true,   
-    default: 0,
-  },
-  image: {
-    type: [String],  // Expecting multiple image paths
-    required: true,
-  },
-  ruler: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  fidbek: {
-    type: Array,
-    required: false,
-  },
-  discount_price: {
-    type: String,
-    required: false,
-    default: 0,
-  },
-  promotion: {
-    type: Boolean,
-    required: false,
-  },
-  bestseller: {
-    type: Boolean,
-    required: false,
-  },
-  oils_type: {
-    type: String,
-    required: false,
-  },
-  product_info_pdf: {
-    type: String,
-    required: true,  // PDF file path
-  }
-});
-
-// Create the Product model
-const Product = mongoose.model('Product', productSchema);
+const Product =require('../models/ProductModels')
 
 const router = express.Router();
 
